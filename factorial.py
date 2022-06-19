d8369520df330e0ff0488bae168ee929b489ec81
S R Reddy Medapati <62867612+srmedapati@users.noreply.github.com>
2022-06-19 19:03:21 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(num):
+    mul=1
+    if num==0:
+        print(1)
+    else:
+        for i in range(1,num+1):
+            mul=mul*i
+        print(mul)
+def __init__=="__main__":
+    num=int(input("Enter any integer : "))
+    print("Factorial of",num," is :")
+    factorial(num)
